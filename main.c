56e0a773669df276ab3eeeb95199f143bc9b73c8
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-03-04 01:44:19 +0700

    varables

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-    printf("hello jokwi\n");
-    
+    int age = 10;
+    float gpa = 3.4;
+    char grade = 'A';
+    char name[] = "jokowi"; //array of charcter
+
+    printf("hello %s \n", name);
+    printf("your age is %d years old\n", age);
+    printf("your gpa is %f\n", gpa);
+    printf("your grade is %c\n", grade);
+
     return 0;
 }
